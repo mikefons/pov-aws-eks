5e7317304c6130cdd53dc6e1fcf5d3f1310fabfc
mikefons <fonseca.michael@gmail.com>
2021-12-22 17:30:38 -0500

    added more vars

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "subnets" {
+  description = "A list of subnets to place the EKS cluster and workers within."
+  type        = list(string)
+  default     = []
+}
+
+variable "cluster_version" {
+  description = "Kubernetes minor version to use for the EKS cluster (for example 1.21)."
+  type        = string
+  default     = null
+}
+
+variable "vpc_id" {
+  description = "VPC where the cluster and workers will be deployed."
+  type        = string
+  default     = null
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster. Also used as a prefix in names of related resources."
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
