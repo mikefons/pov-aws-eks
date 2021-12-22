8d647667b5eac17ffa361262a1bbb9a36d21c3c1
mikefons <fonseca.michael@gmail.com>
2021-12-22 17:33:05 -0500

    added region

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "eks" {
   source  = "app.terraform.io/mfonseca-org/eks/aws"
   version = "16.1.0"
+  region  = var.region
   subnets = var.subnets
   cluster_version = var.cluster_version
   vpc_id  = var.vpc_id
