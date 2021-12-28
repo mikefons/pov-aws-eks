fd9f193f24527bf53bcbc469d41f5ac2c49009f3
mikefons <fonseca.michael@gmail.com>
2021-12-28 13:48:47 -0500

    down graded k8 version

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
-  load_config_file       = false
+  load_config_file       = "false"
 }
 
 module "eks" {
   source  = "app.terraform.io/mfonseca-org/eks/aws"
-  version = "16.1.4"
+  version = "16.1.5"
   subnets = var.subnets
   cluster_version = var.cluster_version
   vpc_id  = var.vpc_id
